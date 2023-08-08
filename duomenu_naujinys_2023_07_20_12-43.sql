1fb22c772cb6d0fb77bd9a39b00a2bd6656d76c2
jolbud <68180668+jolbud@users.noreply.github.com>
2023-08-08 08:53:43 +0300

    Add files via upload

---
+++ duomenu_naujinys_2023_07_20_12-43.sql
@@ -0,0 +1,10 @@
+UPDATE quota_request 
+SET original_requested_amount = 717.000,
+    original_unit = 'NAR',
+    status  = 'ISSUED'
+WHERE id = '7dd4a0f5:1896d7b2562:-7fff';
+UPDATE quota_request 
+SET original_requested_amount = 592.000,
+    original_unit = 'NAR',
+    status  = 'ISSUED'
+WHERE id = '7dd4a0f5:1896d7b2562:-8000';
